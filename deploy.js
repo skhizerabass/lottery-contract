8e498c9a03dbb8285815283b09194efe39ce121f
Syed Khizer Abass <syedkhizerabass@Syeds-MacBook-Pro-2.local>
2022-09-01 22:16:55 +0500

    final contract code

--- deploy.js
+++ deploy.js
@@ -3,9 +3,8 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  'bleak fringe barrel vocal private age glue lend phone repair prefer scene',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
@@ -19,7 +18,8 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
-  provider.engine.stop();
+  // provider.engine.stop();
 };
 deploy();
